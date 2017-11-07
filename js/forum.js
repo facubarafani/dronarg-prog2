f8ae06d236cdbfc35ff8a52d0136c76065aa3116
Facundo Barafani <facu1910@gmail.com>
2017-11-07 07:55:07 -0300

    Se añade base de datos

--- js/forum.js
+++ js/forum.js
@@ -22,16 +22,21 @@ var postTitle = document.getElementById('postitle');
 var postBody = document.getElementById('postbody');
 var newTitle = document.getElementById("newtitle");
 var database = firebase.database();
+var cat;
+
+$('#choosecat').change(function () {
+    cat = $(this).find("option:selected").text();
+});
 
 function submitPost() {
-
     var postTitle = document.getElementById('postitle').value;
     var postBody = document.getElementById('postbody').value;
     var data = {
         title: postTitle,
         body: postBody,
         url: photoUrl,
-        user: name
+        user: name,
+        category: cat
     }
     var posts = database.ref('posts');
     database.ref('posts').push(data);
@@ -67,21 +72,36 @@ function actualizarDb(){
         var img = document.createElement('img');
         var tit = document.createElement('h2');
         var namepost = document.createElement('p');
+        var divf = document.createElement('div');
+        var spanc = document.createElement('span');
+        var replyb = document.createElement('button');
+        var likeb = document.createElement('button');
+        var spanlb = document.createElement('span');
         div.classList = "card text-white bg-dark mb-3";
         divh.classList = "card-header";
         divb.classList = "card-body";
+        divf.classList = "card-footer";
+        spanc.classList = "badge badge-secondary";
+        replyb.classList = "btn btn-info";
         med.append(div);
         div.append(divh);
         div.append(divb);
+        div.append(divf);
         img.src = snap.val().url;
         img.setAttribute("class", "postprofilepic");
         namepost.setAttribute("id", "postn");
+        spanc.setAttribute("id","categorybadge");
+        div.setAttribute("id","divposts");
         tit.innerHTML = snap.val().title;
         namepost.innerHTML = snap.val().user;
         //divh.innerHTML = snap.val().title;
+        spanc.innerHTML = snap.val().category;
+        replyb.innerHTML = "Responder";
         divh.appendChild(tit);
         divh.appendChild(namepost);
         divh.appendChild(img);
+        divf.appendChild(spanc);
+        divf.appendChild(replyb);
         divb.innerHTML = snap.val().body;
         $(".postprofilepic").css("display","block");
     });
